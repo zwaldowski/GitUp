f19ed909a32bbfd650b9b1f3b7cf0d1349f44af3
Zachary Waldowski <zach@waldowski.me>
2019-10-10 02:56:24 -0400

    Have the command-line tool use scripting instead of custom protocol

--- GitUp/Tool/main.m
+++ GitUp/Tool/main.m
@@ -13,19 +13,21 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#import <Foundation/Foundation.h>
-#import <libgen.h>
+#import "GitUp-ScriptingDefinition.h"
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdocumentation"
-#import <git2.h>
-#pragma clang diagnostic pop
+#if DEBUG
+#define kAppBundleIdentifier "co.gitup.mac-debug"
+#else
+#define kAppBundleIdentifier "co.gitup.mac"
+#endif
 
-#import "ToolProtocol.h"
+#define kToolCommand_Help "help"
+#define kToolCommand_Open "open"
+#define kToolCommand_Map "map"
+#define kToolCommand_Commit "commit"
+#define kToolCommand_Stash "stash"
 
-#define kCommunicationTimeOut 3.0
-
-static char* _help = "\
+#define kHelpFormat "\
 Usage: %s [command]\n\
 \n\
 Commands:\n\
@@ -45,101 +47,52 @@ Commands:\n\
 " kToolCommand_Stash "\n\
   Open the current Git repository in GitUp in Stashes view.\n\
 \n\
-";
+"
 
 // We don't care about free'ing resources since the tool is one-shot
 int main(int argc, const char* argv[]) {
-  BOOL success = NO;
   @autoreleasepool {
-    const char* command = argc >= 2 ? argv[1] : "open";
+    NSURL* executableURL = [NSURL fileURLWithFileSystemRepresentation:argv[0] isDirectory:NO relativeToURL:nil];
+    NSString* command = argc >= 2 ? @(argv[1]) : @kToolCommand_Open;
 
-    if (!strcmp(command, kToolCommand_Help)) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wformat-nonliteral"
-      fprintf(stdout, _help, basename((char*)argv[0]));
-#pragma clang diagnostic pop
-      success = YES;
+    if ([command isEqual:@kToolCommand_Help]) {
+      fprintf(stdout, kHelpFormat, executableURL.lastPathComponent.UTF8String);
+      return 0;
     }
 
-    else {
-      assert(git_libgit2_init() >= 1);
-
-      // Find and open repo
-      char wdBuffer[MAXPATHLEN];
-      git_buf repoBuffer = {0};
-      int result = git_repository_discover(&repoBuffer, getwd(wdBuffer), false, NULL);
-      if (result == GIT_OK) {
-        git_repository* repository;
-        result = git_repository_open(&repository, repoBuffer.ptr);
-        if (result == GIT_OK) {
-          if (!git_repository_is_bare(repository)) {
-            const char* path = git_repository_workdir(repository);
-            NSString* repositoryPath = [[NSFileManager defaultManager] stringWithFileSystemRepresentation:path length:strlen(path)];
-
-            // Launch app and / or bring it to front
-            NSString* executablePath = [[[NSBundle mainBundle] executablePath] stringByResolvingSymlinksInPath];  // -executablePath is returning the symlink instead of the actual executable
-            NSString* appPath = [[[executablePath stringByDeletingLastPathComponent] stringByDeletingLastPathComponent] stringByDeletingLastPathComponent];  // Remove "Contents/SharedSupport/{executable}"
-            LSLaunchURLSpec spec = {0};
-            spec.appURL = (__bridge CFURLRef)[NSURL fileURLWithPath:appPath isDirectory:YES];
-            spec.launchFlags = kLSLaunchNoParams | kLSLaunchAndDisplayErrors;
-            OSStatus status = LSOpenFromURLSpec(&spec, NULL);
-            if (status == noErr) {
-              CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
-              while (1) {
-                CFMessagePortRef messagePort = CFMessagePortCreateRemote(kCFAllocatorDefault, CFSTR(kToolPortName));
-                if (messagePort) {
-                  // Send message
-                  NSMutableDictionary* message = [[NSMutableDictionary alloc] init];
-                  [message setObject:[NSString stringWithUTF8String:command] forKey:kToolDictionaryKey_Command];
-                  [message setObject:repositoryPath forKey:kToolDictionaryKey_Repository];
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunguarded-availability"
-                  // The deprecation for this method in the macOS 10.14 SDK
-                  // marks the incorrect version for its introduction. However,
-                  // it is useful to keep availability guards on in general.
-                  // FB6233110
-                  NSData* sendData = [NSKeyedArchiver archivedDataWithRootObject:message];
-#pragma clang diagnostic pop
-                  CFDataRef returnData = NULL;
-                  status = CFMessagePortSendRequest(messagePort, 0, (CFDataRef)sendData, kCommunicationTimeOut, kCommunicationTimeOut, kCFRunLoopDefaultMode, &returnData);
-                  if (status == kCFMessagePortSuccess) {
-                    NSDictionary* response = [NSKeyedUnarchiver unarchiveObjectWithData:(__bridge NSData*)returnData];
-                    NSString* error = [response objectForKey:kToolDictionaryKey_Error];
-                    if (error) {
-                      fprintf(stderr, "%s\n", error.UTF8String);
-                    } else {
-                      success = YES;
-                    }
-                  } else {
-                    fprintf(stderr, "Failed communicating with GitUp application (%i)\n", status);
-                  }
-                  CFMessagePortInvalidate(messagePort);
-                  CFRelease(messagePort);
-                  break;
-
-                } else {
-                  if (CFAbsoluteTimeGetCurrent() >= startTime + kCommunicationTimeOut) {
-                    fprintf(stderr, "Failed connecting to GitUp application\n");
-                    break;
-                  }
-                  usleep(100 * 1000);  // Sleep 100ms and try connecting again
-                }
-              }
-            } else {
-              fprintf(stderr, "Failed launching GitUp application (%i)\n", status);
-            }
-
-          } else {
-            fprintf(stderr, "Bare repositories are not supported at this time\n");
-          }
-        } else {
-          const git_error* error = giterr_last();
-          fprintf(stderr, "Failed opening repository at current path (%s)\n", error ? error->message : NULL);
-        }
-      } else {
-        fprintf(stderr, "No repository found at current path\n");
-      }
+    // Remove "Contents/SharedSupport/{executable}"
+    NSURL* appURL = [[[[executableURL URLByResolvingSymlinksInPath] URLByDeletingLastPathComponent] URLByDeletingLastPathComponent] URLByDeletingLastPathComponent];
+    GitUpApplication* app = [appURL.pathExtension isEqual:@"app"] ? [SBApplication applicationWithURL:appURL] : [SBApplication applicationWithBundleIdentifier:@kAppBundleIdentifier];
+    if (!app) {
+      fprintf(stderr, "Could not find GitUp application\n");
+      return 1;
     }
+
+    // Attempt to open the current directory as a repository.
+    NSString* cwdPath = NSFileManager.defaultManager.currentDirectoryPath;
+    NSURL* cwd = [NSURL fileURLWithPath:cwdPath isDirectory:YES];
+    GitUpDocument* doc = [app open:cwd];
+    if (!doc) {
+      fprintf(stderr, "Failed opening repository at current path (%s)\n", app.lastError.description.UTF8String ?: "unknown");
+      return 1;
+    }
+
+    // If specified, attempt to change the window tab.
+    if ([command isEqual:@kToolCommand_Map]) {
+      doc.mode = GitUpWindowModeMap;
+    } else if ([command isEqual:@kToolCommand_Commit]) {
+      doc.mode = GitUpWindowModeCommit;
+    } else if ([command isEqual:@kToolCommand_Stash]) {
+      doc.mode = GitUpWindowModeStashes;
+    }
+
+    if (app.lastError) {
+      fprintf(stderr, "Failed changing the window mode of the repository (%s)\n", app.lastError.description.UTF8String);
+      return 1;
+    }
+
+    // All ready. Bring it frontmost.
+    [app activate];
+    return 0;
   }
-  return success ? 0 : 1;
 }
